7cf6d10f12371b3b2637b7e45c5d247fe1867ffa
John Albesa <107880782+jnkziaa@users.noreply.github.com>
2022-11-15 11:49:43 -0800

    +++

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template:`
+    <app-header></app-header>
+    <app-home></app-home>
+  `,
+  styles: []
 })
 export class AppComponent {
   title = 'untitled';
